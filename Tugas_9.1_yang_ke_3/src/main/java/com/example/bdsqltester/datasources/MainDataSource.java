3f0105340e846ac3ae1a49ad86ce79ce35926fc7
Wolf-web-dot <wolfydragneel740@gmail.com>
2025-05-08 14:02:10 +0900

    Add files via upload

---
+++ Tugas_9.1_yang_ke_3/src/main/java/com/example/bdsqltester/datasources/MainDataSource.java
@@ -0,0 +1,28 @@
+package com.example.bdsqltester.datasources;
+
+import com.zaxxer.hikari.*;
+
+import java.sql.*;
+
+public class MainDataSource {
+
+    private static HikariConfig config = new HikariConfig();
+    private static HikariDataSource ds;
+
+    static {
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
+        config.setUsername("postgres");
+        config.setPassword("cicing");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        ds = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+
+    private MainDataSource() {
+    }
+}
